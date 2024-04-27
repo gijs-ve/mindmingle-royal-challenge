8fabf9dea07f3c79489b0d8e662aef06d8620edf
AdneCode <super-gijs@hotmail.com>
2024-04-27 12:58:20 +0200

    genesis: start

---
+++ src/lib/LinkItems.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+
+const linkItems = [
+  {
+    href: "https://github.com/gijs-ve/mindmingle-royal-challenge",
+    text: "Source",
+  },
+  {
+    href: "https://www.mindmingle.nl",
+    text: "Mind Mingle",
+  },
+];
+export const LinkItems = () => {
+  return (
+    <div className="absolute bottom-0 flex w-full justify-center gap-4 p-2 text-center">
+      {linkItems.map(({ ...props }, index) => {
+        return <LinkItem {...props} key={index} />;
+      })}
+    </div>
+  );
+};
+const LinkItem = ({ href, text }: { href: string; text: string }) => {
+  return (
+    <Link
+      className="rounded-full border-2 bg-gray-800 p-4 hover:bg-gray-700"
+      href={href}
+    >
+      {text}
+    </Link>
+  );
+};

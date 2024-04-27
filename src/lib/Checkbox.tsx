9827d7df01585fbbd4e1ad939239d42c980653ea
AdneCode <super-gijs@hotmail.com>
2024-04-27 15:05:50 +0200

    feat: background color and willy

--- src/lib/Checkbox.tsx
+++ src/lib/Checkbox.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { motion } from "framer-motion";
-import { useState } from "react";
+import Image from "next/image";
+import { Dispatch, SetStateAction, useState } from "react";
 import { Crown } from "./Crown";
 type Position = { x: number; y: number };
 const getRandomPosition = (xMax: number, yMax: number): Position => {
@@ -12,17 +13,22 @@ const getRandomPosition = (xMax: number, yMax: number): Position => {
 };
 type CheckboxProps = {
   containerDimensions: { width: number; height: number };
+  checked: boolean;
+  setChecked: Dispatch<SetStateAction<boolean>>;
 };
 export const Checkbox = ({
+  checked,
+  setChecked,
   containerDimensions: { width, height },
 }: CheckboxProps) => {
   const [{ x, y }, setPosition] = useState<Position>({
     x: 0,
     y: 0,
   });
-  const transitionPercentage = 0.3;
-  const maxX = width * transitionPercentage;
-  const maxY = height * transitionPercentage;
+  const widthPercentage = 0.3;
+  const heightPercentage = 0.1;
+  const maxX = width * widthPercentage;
+  const maxY = height * heightPercentage;
   return (
     <MotionDiv
       className="flex flex-col justify-items items-center "
@@ -52,10 +58,17 @@ export const Checkbox = ({
       >
         <Crown />
       </MotionDiv>
+      <label className="text-white size-48">
+        <Image alt="willy1" src="/willy/1.jpeg" width={192} height={192} />
+      </label>
       <input
-        onClick={() => setPosition(getRandomPosition(maxX, maxY))}
+        onClick={() => {
+          setPosition(getRandomPosition(maxX, maxY));
+          setChecked((prev) => !prev);
+        }}
+        checked={checked}
         type="checkbox"
-        className="size-48 appearance-none border-orange-900 border-2 rounded bg-red-900 checked:bg-orange-600"
+        className="size-48 appearance-none border-orange-900 border-2 rounded bg-white checked:bg-orange-600"
       />
     </MotionDiv>
   );
--- src/lib/Main.tsx
+++ src/lib/Main.tsx
@@ -1,14 +1,17 @@
 "use client";
 import { Checkbox } from "@/lib/Checkbox";
 import { LinkItems } from "@/lib/LinkItems";
+import { motion } from "framer-motion";
 import { useEffect, useRef, useState } from "react";
 
 export const Main = () => {
   const containerRef = useRef<HTMLElement>(null);
+  const [checked, setChecked] = useState(false);
   const [dimensions, setDimensions] = useState({
     width: 0,
     height: 0,
   });
+
   useEffect(() => {
     if (containerRef.current) {
       setDimensions({
@@ -17,13 +20,28 @@ export const Main = () => {
       });
     }
   }, []);
+  const checkboxProps = {
+    checked,
+    setChecked,
+    containerDimensions: dimensions,
+  };
   return (
-    <main
+    <MotionMain
       ref={containerRef}
+      style={{
+        background: checked
+          ? "#FF4F00"
+          : "linear-gradient(90deg, #f6f6f6 50%, #f6f6f6 50%)",
+      }}
+      animate={{
+        backgroundColor: checked ? "#FF4F00" : "#f6f6f6",
+      }}
       className="flex h-screen bg-slate-500 flex-col items-center justify-center p-24"
     >
-      <Checkbox containerDimensions={dimensions} />
+      <Checkbox {...checkboxProps} />
       <LinkItems />
-    </main>
+    </MotionMain>
   );
 };
+
+const MotionMain = motion.main;
